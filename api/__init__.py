fd2e31415bed353ec6376ef9bf73eeb0b6ced786
Higor Monteiro <higor@Higors-MacBook-Air.local>
2024-04-10 12:07:07 -0300

    fix: precommit to lint

--- api/__init__.py
+++ api/__init__.py
@@ -1,12 +1,10 @@
 from __future__ import annotations
-from django.apps import apps
 
 import os
 
+import django
+
 __version__ = "0.1.0"
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.config.settings")
-
-if not apps.ready:
-    import django
-    django.setup()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings.settings")
+django.setup()
