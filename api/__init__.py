df4bef80172a20fdcd453e3f7a6a8f5fd8755ee9
Higor Monteiro <higor@Higors-MacBook-Air.local>
2024-04-10 12:35:37 -0300

    fix: path settings module

--- api/__init__.py
+++ api/__init__.py
@@ -7,5 +7,5 @@ import django
 
 __version__ = '0.1.0'
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.config.settings')
 django.setup()
